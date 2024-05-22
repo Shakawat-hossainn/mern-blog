3e8ebf6c42679858327adb3e8350bb21ccb56920
Shakawat-hossainn <shakahossain044@gmail.com>
2024-05-22 21:34:45 +0600

    changes in the package

--- Backend/app.js
+++ Backend/app.js
@@ -28,7 +28,7 @@ app.use((err,req,res,next)=>{
 })
 app.use(express.static(path.join(__dirname,'/Frontend/dist')))
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'Frontend','dist','index.html'))
+    res.sendFile(path.join(__dirname,'/Frontend/dist/index.html'))
 })
 const PORT = process.env.PORT || 2000;
 
