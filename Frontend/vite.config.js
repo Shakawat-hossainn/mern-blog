6c0164fa243b4e9984a442984ca2abd8097e85df
Shakawat-hossainn <164717874+Shakawat-hossainn@users.noreply.github.com>
2024-05-22 22:10:02 +0600

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
  
     proxy:{
       '/api':{
-        target:'https://mern-blog-backend-three.vercel.app',
+        target:'https://mern-blog-wklw.onrender.com',
         secure:false
        
       
