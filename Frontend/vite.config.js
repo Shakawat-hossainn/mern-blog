5356eb066bb7f4c9a6bc079db1a4784a7ba85455
Shakawat-hossainn <shakahossain044@gmail.com>
2024-05-23 14:11:27 +0600

    optimized for production

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,16 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
- 
-    proxy:{
-      '/api':{
-        target:'https://mern-blog-backend-ten-xi.vercel.app',
-        secure:false
-       
-      
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: process.env.PORT || 5173, // Use the port provided by Render or default to 5173
+    proxy: {
+      '/api': {
+        target: 'https://mern-blog-backend-ten-xi.vercel.app',
+        secure: false
       }
     }
   },
-  plugins: [react()],
+  plugins: [react()]
 })
